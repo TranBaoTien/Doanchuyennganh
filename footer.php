0e85d5ef473b09f1e5f0f2fb439fd95b495fad95
TranBaoTien <94682185+TranBaoTien@users.noreply.github.com>
2021-11-19 17:30:15 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer>
+<div class="">
+				<div >
+					<h3>Liên hệ với chúng tôi</h3>
+					<ul >
+						
+						<li><span>Địa chỉ:</span> 155 Trần Hưng Đạo Long Thạnh Thị Xã Tân Châu An Giang</li>
+						<li ><span >Email:</span > tranbaotien1182000@gmail.com</li>
+						<li><span>Điện thoại:</span> 0902214601</li>
+						<li><span>Zalo:</span> 0902214601</li>
+						
+					</ul>
+				</div>
+				
+</div>
+<div class="copyright clearfix">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<span>© Bản quyền thuộc về <b>Đồ Nghề Tóc</b> <span class="s480-f">
+					
+				</div>
+			</div>
+			
+			
+	</div>
+</footer>
---
+++ header.php
@@ -0,0 +1,68 @@
+<header class="header">
+    <div class="topheader">
+        <div class="container">
+			<div class="row">
+				<div class="logo">
+					
+				</div>
+				<div class="navdau">
+					
+					<a href="#" title="Tìm kiếm">Trang chủ</a>
+					
+					<a href="#" title="Đăng nhập">Đăng nhập</a>
+					
+					<a href="#" title="Đăng ký">Đăng ký</a>
+					
+					<a href="#" title="Giỏ hàng">Giỏ hàng</a>
+					
+					<a href="#" title="ĐĂNG KÝ KHÓA HỌC BARBER">Khóa Học</a>
+					
+				</div>
+			</div>
+    </div>
+</div>
+<div class="topheader">
+        <div class="container">
+			<div class="row">
+				<div class="logo">
+					
+				</div>
+				<div class="navdau">
+					
+					<a href="#" title="Tìm kiếm">Trang chủ</a>
+					
+					<a href="#" title="Đăng nhập">Sản Phẩm</a>
+					
+					<a href="#" title="Đăng ký">Đăng ký</a>
+					
+					<a href="#" title="Giỏ hàng">Giỏ hàng</a>
+					
+					<a href="#" title="ĐĂNG KÝ KHÓA HỌC BARBER">Khóa Học</a>
+					
+				</div>
+		</div>
+    </div>
+</div>
+
+<div class="topheader">
+        <div class="container">
+			<div class="row">
+				<div class="logo">
+					
+				</div>
+				<div class="navdau">
+					
+				<ul>
+						<li class="search">
+						    <input type="text" placeholder="Search..">
+						    <button class="btn btn-primary" type="button">Tim</button>				
+                        </li>
+					
+				</ul>
+					
+				</div>
+			
+		</div>
+    </div>
+</div>
+</header>
