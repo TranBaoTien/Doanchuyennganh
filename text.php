2c93703fa6f1c0772c54af0ffe20695b93926459
TranBaoTien <94682185+TranBaoTien@users.noreply.github.com>
2021-11-19 17:20:03 +0700

    Add files via upload

---
+++ text.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/style.css">
+    
+<style>
+
+
+    
+</style>
+</head>
+<body>
+    <header class="header">
+        
+        
+        <div class="topheader">
+            <div class="container">
+                    <div class="row">
+                        <div class="logo" style="float:left;">
+                            <img src="img/abc.ipeg">
+                        </div>
+                        <div class="sreach" style="float:left;">
+                          <li >
+                            <input type="text" placeholder="Search..">
+                            <button class="btn btn-primary" type="button">Tìm</button>				
+                          </li>
+                        </div>
+                        <div class="dropdown" style="">
+                                <a class="dropbtn">Giỏ Hàng</a>
+                        </div>
+                        <div class="dropdown" style="">
+                                <a class="dropbtn">Trang Chủ</a>
+                        </div>
+                        <div class="dropdown" style="">
+                                <a class="dropbtn">Sản Phẩm</a>
+                                <div class="dropdown-content" style="left: 20px;">
+                                    <a href="#">Tông đơ </a>
+                                    <a href="#">Kéo</a>
+                                    <a href="#">Wax</a>
+                                </div>
+                        </div>
+                        <div class="dropdown" style="">
+                                <a class="dropbtn">Bài trước</a>
+                                <div class="dropdown-content" style="left: 20px;">
+                                    <a href="#">Float và Clear</a>
+                                    <a href="#">Combinator</a>
+                                    <a href="#">Pseudo-Class</a>
+                                </div>
+                        </div>
+                    </div>
+                
+            </div>
+        </div>
+        <div class="topheader">
+            <div class="container">
+                    <div class="row">
+                        
+                        <div class="dropdown" style="">
+                                <a class="dropbtn">Tông đơ</a>
+                                <div class="dropdown-content" >
+                                    <a href="#">Tông đơ có dây</a>
+                                    <a href="#">Tông đơ không dây</a>
+                                </div>
+                        </div>
+                        <div class="dropdown" style="">
+                                <a class="dropbtn">Máy Làm Tóc</a>
+                                <div class="dropdown-content">
+                                    <a href="#">Máy sấy tóc</a>
+                                    <a href="#">Máy cạo râu</a>
+                                  
+                                </div>
+                        </div>
+                        <div class="dropdown" style="">
+                                <a class="dropbtn">Kéo Cắt Tóc</a>
+                                <div class="dropdown-content" >
+                                    <a href="#">Kéo cắt</a>
+                                    <a href="#">Kéo tỉa</a>
+                                    <a href="#">Kéo cặp</a>
+                                </div>
+                        </div>
+                        <div class="dropdown" style="">
+                                <a class="dropbtn">Phụ Kiện</a>
+                                <div class="dropdown-content" >
+                                    <a href="#">Áo choàng</a>
+                                    <a href="#">Đèn ráy tay</a>
+                                    <a href="#">Bình xịt</a>
+                                </div>
+                        </div>
+                        <div class="dropdown" style="">
+                                <a class="dropbtn">Phụ Liệu Tóc</a>
+                                <div class="dropdown-content">
+                                    <a href="#">Wax</a>
+                                    <a href="#">Thuốc nhộm</a>
+                                    
+                                </div>
+                        </div>
+                    </div>
+                
+            </div>
+        </div>
+           
+</header>
+<footer>
+<?php include "layout/footer.php"?>
+
+
+</footer>
+</body>
+</html>
+
